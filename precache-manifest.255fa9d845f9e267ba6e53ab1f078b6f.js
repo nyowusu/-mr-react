1c38ad5fb5c248e98fae199c2a5e24c83dc67856
nyowusu <yaw.owusu@hotmail.com>
2019-07-14 23:56:46 +0100

    Updates

---
+++ precache-manifest.255fa9d845f9e267ba6e53ab1f078b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e17fdb49cbc09b239e2cb2799c43b4f",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "f5cf7a4c91ed101e4cb3",
+    "url": "/monsters-react/static/css/main.e97dedeb.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/monsters-react/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "f5cf7a4c91ed101e4cb3",
+    "url": "/monsters-react/static/js/main.cca3d551.chunk.js"
+  },
+  {
+    "revision": "e94f20c01308e085c048",
+    "url": "/monsters-react/static/js/runtime~main.56780d6f.js"
+  }
+]);
